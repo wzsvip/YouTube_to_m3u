c7df600e50cc8dc756d18e15d0cf5a54797ad144
wzsvip <161473552+wzsvip@users.noreply.github.com>
2024-10-13 21:15:15 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -6,15 +6,21 @@ windows = False
 if 'win' in sys.platform:
     windows = True
 
+def get_url_content(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Mobile Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+    content = response.text
+    return content
+    
 def grab(url):
     response = requests.get(url, timeout=15).text
     if '.m3u8' not in response:
-        #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
                 print('https://raw.githubusercontent.com/Kentsonshum/YouTube_to_m3u/main/assets/info.m3u')
                 return
-            #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
@@ -33,8 +39,7 @@ def grab(url):
     print(f"{link[start : end]}")
 
 print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
-print(banner)
-#s = requests.Session()
+# banner变量未定义，需要补充或删除此行
 with open('../youtube_channel_info.txt') as f:
     for line in f:
         line = line.strip()
